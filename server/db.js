81e48da7965dc7e597300580ca9dda16f87c456c
John Doe <lambett@mail.ru>
2025-09-23 10:34:18 +0300

    push

--- server/db.js
+++ server/db.js
@@ -1,12 +1,14 @@
-import { Pool } from "pg";
-import dotenv from 'dotenv'
-dotenv.config()
+const {Sequelize} = require('sequelize')
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, 
-    ssl: 
-        process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-        
-});
 
-export default pool;
+
+module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+)
