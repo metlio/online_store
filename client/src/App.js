b555305181e5ed58eb16b8392cf7f38c92692002
metlio <164861524+metlio@users.noreply.github.com>
2025-09-25 16:52:27 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,21 @@ import React, {useContext, useEffect, useState} from 'react';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
-//import {NavLink} from "react-router-dom";
 import {observer} from "mobx-react-lite";
 import {Context} from "./index";
 import {check} from "./http/userAPI";
 import {Spinner} from "react-bootstrap";
+import jwt_decode from "jwt-decode";
 import Happ from './Happ'
-//import Counter from './components/Counter';
 import Preorder from './components/Preorder';
 import Footer from './components/Layout/Footer';
 import './components/Courusel/Courusel.css';
 import './pages/Tvorog.css'
-//import { Courusel } from './components/Courusel/Courusel'
-//import Spa from './spa/Spa';
 import CartContextProvider from "./store/CartContextProvider";
-import MainFill from './components/MainFill';
-import MainFill2 from './components/MainFill2';
-//import SpringDemo from './components/SpringDemo';
-//import Vesna from './components/Vesna';
-//import Gulp from './components/MainFill2';
-//import PullRelease from './components/MainFill2';
-import Logo from './components/Logo';
 import Menu from './components/Menu';
 import TypeBar from './components/TypeBar';
 import UserBar from './components/UserBar';
 import Other from './pages/Other';
-import MainTheme4 from './components/MainTheme4';
 
 const App = observer(() => {
     const {user} = useContext(Context)
@@ -35,9 +24,21 @@ const App = observer(() => {
 
     useEffect(() => {
         check().then(data => {
-            user.setUser(data);
-            user.setIsAuth(true)
-        }).catch(e => console.log(e)).finally(() => setLoading(false))
+            if (data) {
+                try {
+                    const decodedUser = jwt_decode(data.token);
+                    user.setUser(decodedUser);
+                    user.setIsAuth(true);
+                } catch (e) {
+                    console.error("Failed to decode token on app load:", e);
+                    localStorage.removeItem('token');
+                }
+            }
+        }).catch(e => {
+            console.error("Failed to check auth on load:", e);
+        }).finally(() => {
+            setLoading(false);
+        });
     }, [])
 
     if (loading) {
@@ -45,42 +46,24 @@ const App = observer(() => {
     }
 
     return (
-    <div style={{width:'100vw', top:'0px', height:'200vh', backgroundColor:'#fff'}}> 
+    <div style={{width:'100vw', top:'0px', height:'200vh', backgroundColor:'#fff'}}>
         <CartContextProvider>
         <BrowserRouter>
-
         <Menu />
         <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around',backgroundColor:'#fff'}}>
         <TypeBar />
         <UserBar />
         </div>
-            {/* <SpringDemo/> */}
             <Happ />
-
-            {/* <MainFill /> */}
-            
             <div style={{backgroundColor:'white'}}>
             <AppRouter />
             </div>
-            {/* <Vesna /> */}
-
-
-
-            
-            
-            { /*<Courusel>
-                <div className='item item-1'>Добро пожаловать</div>
-                <div className='item item-2'>Вторая надпись</div>
-                <div className='item item-3'>Третья надпись</div>
-            </Courusel> */}
         </BrowserRouter>
-        {/* <Footer /> */}
                 <Preorder/>
-
         <Footer />
         <Other />
         </CartContextProvider>
-    </div>    
+    </div>
     );
 });
 
