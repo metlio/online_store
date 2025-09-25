2a8f095653026a6f07f6df840ed6838606debb67
metlio <164861524+metlio@users.noreply.github.com>
2025-09-25 16:50:26 +0300

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,12 +5,9 @@ module.exports = function (req, res, next){
         next()
     }
     try {
-        if (!req.headers.authorization) {
-            return res.status(401).json({message:"Не авторизован"});
-        }
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token || token === 'null') {
-            return res.status(401).json({message:"Не авторизован"})
+        const token = req.headers.authorization?.split(' ')[1]
+        if (!token) {
+            return res.status(401).json({message: "Не авторизован"})
         }
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
         req.user = decoded
