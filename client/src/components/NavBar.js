e3701f99f76b1c221e82ee493a2db1a786d03cdc
metlio <164861524+metlio@users.noreply.github.com>
2025-09-25 16:53:28 +0300

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,54 +1,44 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
-//import Navbar from "react-bootstrap/Navbar";
-//import {Row} from "react-bootstrap";
-//import {NavLink} from "react-router-dom";
 import {ADMIN_ROUTE, LOGIN_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
-//import Container from "react-bootstrap/Container";
 import {useNavigate} from 'react-router-dom'
-//import Header from './Layout/Header';
-import styles from './NavBar.module.css'
-//import Logo from './Logo'
-import jwt_decode from "jwt-decode";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
     const history = useNavigate()
-    let userinfo = jwt_decode(localStorage.token)
-    const user_mail = userinfo.email
+
     const logOut = () => {
         user.setUser({})
         user.setIsAuth(false)
+        localStorage.removeItem('token')
     }
 
     return (
-        <div>   
-                    {user.isAuth ?
-                        <div style={{display:'flex', borderRadius:'5px', backgroundColor:'#fff'}}>
-                     
-                                <button
-                                    style={{textAlign:'center',height:'25px', width:'65px', fontSize: '0.6rem'}} 
-                                    onClick={() => history(ADMIN_ROUTE)}
-                                >
-                                    Админка
-                                </button>
-
-                                <button
-                                    style={{textAlign:'center',height:'25px', width:'60px', fontSize: '0.6rem'}} 
-                                    onClick={() => logOut()}
-                                >
-                                        Выйти
-                                </button>
-                        </div>
-                        :
-                        <div>
-                            <button variant={"outline-light"} style={{fontSize: '0.5rem', color: "black", borderColor:'black'}} onClick={() => history(LOGIN_ROUTE)}>
-                                Авторизация
-                            </button>
-                        </div>
-                    }
-                </div>    
+        <div>
+            {user.isAuth ?
+                <div style={{display:'flex', borderRadius:'5px', backgroundColor:'#fff'}}>
+                    <button
+                        style={{textAlign:'center',height:'25px', width:'65px', fontSize: '0.6rem'}}
+                        onClick={() => history(ADMIN_ROUTE)}
+                    >
+                        Админка
+                    </button>
+                    <button
+                        style={{textAlign:'center',height:'25px', width:'60px', fontSize: '0.6rem'}}
+                        onClick={() => logOut()}
+                    >
+                        Выйти
+                    </button>
+                </div>
+                :
+                <div>
+                    <button variant={"outline-light"} style={{fontSize: '0.5rem', color: "black", borderColor:'black'}} onClick={() => history(LOGIN_ROUTE)}>
+                        Авторизация
+                    </button>
+                </div>
+            }
+        </div>
     );
 });
 
