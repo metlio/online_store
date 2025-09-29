9778cfdc5ebe7e4d8148819ed6d7ca785683ebe8
metlio <164861524+metlio@users.noreply.github.com>
2025-09-29 14:06:39 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {check} from "./http/userAPI";
 import {Spinner} from "react-bootstrap";
 import jwt_decode from "jwt-decode";
 import Happ from './Happ'
+import Other from './pages/Other';
 import Preorder from './components/Preorder';
 import Footer from './components/Layout/Footer';
 import './components/Courusel/Courusel.css';
@@ -61,6 +62,7 @@ const App = observer(() => {
             <AppRouter />
             </div>
         </BrowserRouter>
+        <Other />
         <Footer />
         </CartContextProvider>
     </div>
